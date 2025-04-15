4a6af35321774b2788faec7207b5f8cf07e3ef5b
Punit Turlapati <Punitsst@gmail.com>
2025-04-14 22:35:11 -0500

    Switch from Decimal to float for efficiency

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-from decimal import Decimal
 import math
 import multiprocessing
 from dataclasses import dataclass
@@ -10,29 +9,29 @@ from typing import Generator
 
 @dataclass
 class Result:
-    temperature: Decimal
-    average_energy: Decimal
+    temperature: float
+    average_energy: float
 
 
 # EDITABLE CONSTANTS
 
-kB = Decimal(1)  # Boltzmann constant
-SIZE = 20  # Size of the grid
-J = Decimal(-1)  # Interaction energy
+kB = 1  # Boltzmann constant
+SIZE = 100  # Size of the grid
+J = -1  # Interaction energy
 
 
 def simulation(model: ising.Model, show_graph: bool = False) -> Result:
     energy_list = [model.normalised_energy]
     print(f"Starting simulation: size={model.size} T={model.temperature} J={model.j} kB={model.kB}")
-    for _ in range(300):
+    for _ in range(1000):
         for i, row in enumerate(model.grid):
             for j, _ in enumerate(row):
                 delta = model.calculate_delta(i, j)
 
-                if delta <= Decimal(0):
+                if delta <= 0:
                     model.flip_spin(i, j)
                 else:
-                    probability = Decimal(math.e) ** (-model.beta * delta)
+                    probability = math.e ** (-model.beta * delta)
                     random_value = random.random()
 
                     if random_value < probability:
@@ -40,11 +39,11 @@ def simulation(model: ising.Model, show_graph: bool = False) -> Result:
 
         energy_list.append(model.normalised_energy)
 
-    average_energy = sum(energy_list[-100:]) / Decimal(100)
+    average_energy = sum(energy_list[-100:]) / 100
     result = Result(temperature=model.temperature, average_energy=average_energy)
 
     if show_graph:
-        plt.plot(range(len(energy_list)), [float(i) for i in energy_list], label=f"Simulation at T={model.temperature}")
+        plt.plot(range(len(energy_list)), [i for i in energy_list], label=f"Simulation at T={model.temperature}")
         plt.xlabel("Iterations")
         plt.ylabel("Energy")
         plt.title("Energy vs Iterations")
@@ -60,7 +59,7 @@ def model_vary_temperature(minimum_temperature, maximum_temperature) -> Generato
     # If show_graph is True, then a graph will be shown for that temperature
     # Graph will then need to be closed manually for the simulation to continue
     for t in range(minimum_temperature, maximum_temperature + 1):
-        yield (ising.Model(SIZE, ising.alternating_state, Decimal(t), kB, J), False)
+        yield (ising.Model(SIZE, ising.alternating_state, t, kB, J), False)
 
 
 if __name__ == "__main__":
@@ -68,4 +67,4 @@ if __name__ == "__main__":
     results = pool.starmap(simulation, model_vary_temperature(1, 100))
 
     with open("results.txt", "w+") as file:
-        file.write("\n".join(f"{result.temperature}, {result.average_energy}" for result in results))
+        file.write("\n".join(f"{result.temperature}, {round(result.average_energy, 6)}" for result in results))
