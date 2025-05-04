01c50689a85261e1b674bf1fb35d4edfc98bf591
Lana Herkenhoff <lshbc8@umsystem.edu>
2025-05-04 10:31:40 -0500

    turned to scatter plots and added regression lines

--- plot2.py
+++ plot2.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 import csv
+import numpy as np
+from scipy.optimize import curve_fit
 
 
 # Plotting the energy vs iterations
@@ -14,24 +16,49 @@ with open("exponents.txt", "r") as tve:
         temperature.append(float(line[1]))
         susceptibility.append(float(line[2]))
 
-    fig, axes = plt.subplots(nrows=2)
-    plt.tight_layout()
-    axes[0].plot(size, temperature, label="Tem")
-    axes[0].set_xlabel("Size")
-    axes[0].set_ylabel("Specific Heat")
-    axes[0].set_title("Specific Heat vs Size")
-    axes[0].legend()
-    axes[0].set_yscale('log')
-    axes[0].set_xscale('log')
-
-    axes[1].plot(size, susceptibility, label="Susceptibility")
-    axes[1].set_xlabel("Size")
-    axes[1].set_ylabel("Susceptibility")
-    axes[1].set_title("Susceptibility vs Size")
-    axes[1].legend()
-    axes[1].grid()
-    axes[1].set_yscale('log')
-    axes[1].set_xscale('log')
+def power_law(x, a, b):
+    return a * np.power(x, b)
 
 
-    plt.show()
+
+fig, axes = plt.subplots(nrows=2)
+plt.tight_layout()
+axes[0].scatter(size, temperature, label="Temperature")
+axes[0].set_xlabel("Size")
+axes[0].set_ylabel("Specific Heat")
+axes[0].set_title("Specific Heat vs Size")
+
+size = np.array(size)
+temperature = np.array(temperature)
+
+popt, pcov = curve_fit(power_law, size, temperature)
+a, b = popt 
+
+x_fit = np.linspace(min(size), max(size), 100)
+y_fit = power_law(x_fit, a, b)
+axes[0].plot(x_fit, y_fit, label=f'y = {a:.2f}x**{b:.2f}')
+
+axes[1].scatter(size, susceptibility, label="Susceptibility")
+
+axes[0].legend()
+axes[0].set_yscale('log')
+axes[0].set_xscale('log')
+
+axes[1].set_xlabel("Size")
+axes[1].set_ylabel("Susceptibility")
+axes[1].set_title("Susceptibility vs Size")
+
+popt, pcov = curve_fit(power_law, size, susceptibility)
+a, b = popt 
+
+x_fit = np.linspace(min(size), max(size), 100)
+y_fit = power_law(x_fit, a, b)
+axes[1].plot(x_fit, y_fit, label=f'y = {a:.2f}x**{b:.2f}')
+
+axes[1].legend()
+axes[1].grid()
+axes[1].set_yscale('log')
+axes[1].set_xscale('log')
+
+
+plt.show()
